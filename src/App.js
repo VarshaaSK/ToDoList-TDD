f5175f1cc03f73efc1ab8e966fd331c7e7c1ee3d
VarshaaSK <varshaa.sk@m2pfintech.com>
2023-05-09 15:07:00 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Heading from './Components/Heading'
+import ToDo from './Components/Todo'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route>
+          <Heading/>
+          <ToDo/>
+        </Route>
+      </Routes>
     </div>
   );
 }
